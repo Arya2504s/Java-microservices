d974aea2cb694d87be435e0962282ca7d3fdcdf7
Arya Sharma <73757085+Arya2504s@users.noreply.github.com>
2022-07-21 13:50:56 -0400

    Completed a few API

--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/MongoDao.java
@@ -1,18 +1,110 @@
 package ca.utoronto.utm.mcs;
 
-import com.mongodb.client.MongoCollection;
+import com.mongodb.client.*;
+import com.mongodb.client.model.Filters;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MongoDao {
 	
 	public MongoCollection<Document> collection;
-
+	Dotenv dotenv = Dotenv.load();
+	private final String addr = dotenv.get("MONGODB_ADDR");
+	private final String username = "root";
+	private final String password = "123456";
+	private final String uri = "mongodb://%s:%s@" + addr + ":27017";
+	private final String uri2 = "mongodb+srv://Arya:arya12@cluster0.ehq1k.mongodb.net/?retryWrites=true&w=majority";
+	private final String uriDb = String.format(uri,username, password);
 	public MongoDao() {
         // TODO: 
         // Connect to the mongodb database and create the database and collection. 
         // Use Dotenv like in the DAOs of the other microservices.
+		try {
+			MongoClient mongo = MongoClients.create(this.uri2);
+			MongoDatabase database = mongo.getDatabase("trip");
+			System.out.println(database);
+			this.collection = database.getCollection("trips");
+			System.out.println(this.collection);
+			System.out.println("Connection Successful");
+		}catch(Exception e){
+			System.out.println("Connection unsuccessful");
+		}
 	}
 
 	// *** implement database operations here *** //
+
+	public JSONArray getUserTrips(String uid, String type) throws Exception  {
+		FindIterable<Document> res = this.collection.find(Filters.eq(type,uid));
+		try {
+			JSONArray result = new JSONArray();
+			for (Document doc : res) {
+				doc.put("_id",doc.getObjectId("_id").toString());
+				doc.remove(type);
+				result.put(doc);
+			}
+			return result;
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public JSONObject createTrip(int startTime, String driver, String passenger) throws JSONException {
+		Document doc = new Document();
+		doc.put("startTime", startTime);
+		doc.put("driver", driver);
+		doc.put("passenger", passenger);
+
+		try{
+			this.collection.insertOne(doc);
+			JSONObject result = new JSONObject();
+			result.put("$oid", doc.getObjectId("_id"));
+			return result;
+		}catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public boolean updateTrip(int endTime, String totalCost, int timeElapsed, int distance, ObjectId id){
+		Document doc = new Document();
+		doc.put("distance", distance);
+		doc.put("endTime", endTime);
+		doc.put("timeElapsed", timeElapsed);
+		doc.put("totalCost", totalCost);
+		try{
+			boolean exist = this.tripExists(id);
+			if(!exist){
+				return false;
+			}
+		}catch (Exception e) {
+			throw e;
+		}
+		try {
+			this.collection.updateOne(Filters.eq("_id", id), new Document("$set", doc));
+			return true;
+		}catch (Exception e) {
+			throw e;
+		}
+	}
+
+	public boolean tripExists(ObjectId id){
+	FindIterable<Document> res = this.collection.find(Filters.eq("_id",id));
+		try {
+			JSONArray result = new JSONArray();
+			for (Document doc : res) {
+				doc.put("_id", doc.getObjectId("_id").toString());
+				result.put(doc);
+			}
+			if(result.length() == 0){
+				return false;
+			}
+			return true;
+		}catch (Exception e) {
+			throw e;
+		}
+	}
 
 }
--- TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Passenger.java
+++ TripinfoMicroservice/src/main/java/ca/utoronto/utm/mcs/Passenger.java
@@ -1,7 +1,10 @@
 package ca.utoronto.utm.mcs;
 
 import com.sun.net.httpserver.HttpExchange;
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class Passenger extends Endpoint {
@@ -14,7 +17,41 @@ public class Passenger extends Endpoint {
      */
 
     @Override
-    public void handleGet(HttpExchange r) throws IOException,JSONException{
+    public void handleGet(HttpExchange r) throws IOException,JSONException {
         // TODO
+        try{
+            String[] splitUrl = r.getRequestURI().getPath().split("/");
+            if (splitUrl.length != 4) {
+                this.sendStatus(r, 400);
+                return;
+            }
+            // check if uid given is integer, return 400 if not
+            String uidString = splitUrl[3];
+            int uid;
+            try {
+                uid = Integer.parseInt(uidString);
+            } catch (Exception e) {
+                e.printStackTrace();
+                this.sendStatus(r, 400);
+                return;
+            }
+
+            JSONArray rs;
+            rs = this.dao.getUserTrips(uidString, "passenger");
+            if(0 == rs.length()){
+                this.sendStatus(r, 404);
+                return;
+            }
+            JSONObject var = new JSONObject();
+            JSONObject trips = new JSONObject();
+            trips.put("trips", rs);
+            var.put("data", trips);
+            this.sendResponse(r, var, 200);
+            return;
+        }catch(Exception e) {
+            e.printStackTrace();
+            this.sendStatus(r, 500);
+        }
     }
+
 }
