b8ef68cc8b03be4108d317a3093d8afb8524c2c1
Yuto Omachi <youomachi@gmail.com>
2022-07-22 16:55:56 -0400

    location test is done

--- locationmicroservice/src/main/java/ca/utoronto/utm/mcs/App.java
+++ locationmicroservice/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -5,7 +5,7 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
 public class App {
-    static int PORT = 8200; // TODO: take this back to 8000
+    static int PORT = 8000; // TODO: take this back to 8000
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
         server.createContext("/location/user", new User());
--- locationmicroservice/src/test/java/ca/utoronto/utm/mcs/AppTest.java
+++ locationmicroservice/src/test/java/ca/utoronto/utm/mcs/AppTest.java
@@ -25,41 +25,86 @@ public class AppTest {
 
         try {
             HttpClient client = HttpClient.newHttpClient();
-            String body = "{\"uid\": \"passenger\", \"is_driver\": \"false\"}";
+            String body = "{\"uid\": \"passenger\", \"is_driver\": false}";
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://0.0.0.0:8200/location/user"))
+                .uri(URI.create("http://0.0.0.0:8000/location/user"))
                 .header("Content-Type", "application/json")
-                .PUT(BodyPublishers.ofString(body))
+                .method("PUT", BodyPublishers.ofString(body))
                 .build();
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.statusCode());
 
             client = HttpClient.newHttpClient();
-            body = "{\"uid\": \"driver\", \"is_driver\": \"true\"}";
+            body = "{\"uid\": \"driver\", \"is_driver\": true}";
             request = HttpRequest.newBuilder()
-                .uri(URI.create("http://0.0.0.0:8200/location/user"))
+                .uri(URI.create("http://0.0.0.0:8000/location/user"))
                 .header("Content-Type", "application/json")
-                .PUT(BodyPublishers.ofString(body))
+                .method("PUT", BodyPublishers.ofString(body))
                 .build();
             response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.statusCode());
 
             client = HttpClient.newHttpClient();
-            body = "{\"longitude\": \"1.25\", \"latitude\": \"1.25\", \"street\": \"street1\"}";
+            body = "{\"longitude\": 1.25, \"latitude\": 1.25, \"street\": \"street1\"}";
             request = HttpRequest.newBuilder()
-                .uri(URI.create("http://0.0.0.0:8200/location/passenger"))
-                .header("Content-Type", "application/json")
-                .method("PATCH", BodyPublishers.ofString(body))
-                .build();
-            response = client.send(request, BodyHandlers.ofString());
-
-            client = HttpClient.newHttpClient();
-            body = "{\"longitude\": \"1.24\", \"latitude\": \"1.24\", \"street\": \"street2\"}";
-            request = HttpRequest.newBuilder()
-                .uri(URI.create("http://0.0.0.0:8200/location/driver"))
+                .uri(URI.create("http://0.0.0.0:8000/location/passenger"))
                 .header("Content-Type", "application/json")
                 .method("PATCH", BodyPublishers.ofString(body))
                 .build();
             response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.statusCode());
 
+            client = HttpClient.newHttpClient();
+            body = "{\"longitude\": 1.24, \"latitude\": 1.24, \"street\": \"street2\"}";
+            request = HttpRequest.newBuilder()
+                .uri(URI.create("http://0.0.0.0:8000/location/driver"))
+                .header("Content-Type", "application/json")
+                .method("PATCH", BodyPublishers.ofString(body))
+                .build();
+            response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+
+            client = HttpClient.newHttpClient();
+            body = "{\"roadName\": \"street1\", \"hasTraffic\": true}";
+            request = HttpRequest.newBuilder()
+                .uri(URI.create("http://0.0.0.0:8000/location/road"))
+                .header("Content-Type", "application/json")
+                .method("PUT", BodyPublishers.ofString(body))
+                .build();
+            response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+
+            client = HttpClient.newHttpClient();
+            body = "{\"roadName\": \"street2\", \"hasTraffic\": true}";
+            request = HttpRequest.newBuilder()
+                .uri(URI.create("http://0.0.0.0:8000/location/road"))
+                .header("Content-Type", "application/json")
+                .method("PUT", BodyPublishers.ofString(body))
+                .build();
+            response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+
+            client = HttpClient.newHttpClient();
+            body = "{\"roadName1\": \"street2\", \"roadName2\": \"street1\","
+                + " \"hasTraffic\": true, \"time\": 10}";
+            request = HttpRequest.newBuilder()
+                .uri(URI.create("http://0.0.0.0:8000/location/hasRoute"))
+                .header("Content-Type", "application/json")
+                .method("POST", BodyPublishers.ofString(body))
+                .build();
+            response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+
+            client = HttpClient.newHttpClient();
+            body = "{\"roadName1\": \"street1\", \"roadName2\": \"street2\","
+                + " \"hasTraffic\": true, \"time\": 7}";
+            request = HttpRequest.newBuilder()
+                .uri(URI.create("http://0.0.0.0:8000/location/hasRoute"))
+                .header("Content-Type", "application/json")
+                .method("POST", BodyPublishers.ofString(body))
+                .build();
+            response = client.send(request, BodyHandlers.ofString());
+            System.out.println(response.statusCode());
 
         }
         catch (Exception e){
@@ -69,31 +114,74 @@ public class AppTest {
 
     @AfterAll
     public static void teardown() throws IOException, InterruptedException {
+//        try {
+//            HttpClient client = HttpClient.newHttpClient();
+//            String body = "{\"uid\": \"passenger\"}";
+//            HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("http://0.0.0.0:8200/location/user"))
+//                .header("Content-Type", "application/json")
+//                .method("DELETE", BodyPublishers.ofString(body))
+//                .build();
+//            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+//
+//            client = HttpClient.newHttpClient();
+//            body = "{\"uid\": \"driver\"}";
+//            request = HttpRequest.newBuilder()
+//                .uri(URI.create("http://0.0.0.0:8200/location/user"))
+//                .header("Content-Type", "application/json")
+//                .method("DELETE", BodyPublishers.ofString(body))
+//                .build();
+//            response = client.send(request, BodyHandlers.ofString());
+//
+//            client = HttpClient.newHttpClient();
+//            body = "{\"roadName1\": \"street2\", \"roadName2\": \"street1\"}";
+//            request = HttpRequest.newBuilder()
+//                .uri(URI.create("http://0.0.0.0:8200/location/route"))
+//                .header("Content-Type", "application/json")
+//                .method("DELETE", BodyPublishers.ofString(body))
+//                .build();
+//            response = client.send(request, BodyHandlers.ofString());
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+    }
+
+    @Test
+    public void nearbyDriverPass() {
         try {
             HttpClient client = HttpClient.newHttpClient();
-            String body = "{\"uid\": \"passenger\"}";
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://0.0.0.0:8200/location/user"))
+                .uri(URI.create("http://0.0.0.0:8000/location/nearbyDriver/passenger?radius=100"))
                 .header("Content-Type", "application/json")
-                .method("DELETE", BodyPublishers.ofString(body))
+                .GET()
                 .build();
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-
-            client = HttpClient.newHttpClient();
-            body = "{\"uid\": \"driver\"}";
-            request = HttpRequest.newBuilder()
-                .uri(URI.create("http://0.0.0.0:8200/location/user"))
-                .header("Content-Type", "application/json")
-                .method("DELETE", BodyPublishers.ofString(body))
-                .build();
-            response = client.send(request, BodyHandlers.ofString());
-        } catch (Exception e){
-            e.printStackTrace();
+            System.out.println("nearByPass: " + response.statusCode() + response.body());
+            assertTrue(response.statusCode()==200);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            assertEquals(e.getMessage(), "error");
         }
     }
+
     @Test
-    public void exampleTest() {
-        assertTrue(true);
+    public void nearbyDriverFail() {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://0.0.0.0:8000/location/nearbyDriver/passenger"))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            System.out.println("nearByFail: " + response.statusCode() + response.body());
+            assertTrue(response.statusCode() == 400);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            assertEquals(e.getMessage(), "error");
+        }
     }
 
     @Test
@@ -101,13 +189,32 @@ public class AppTest {
         try {
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://0.0.0.0:8200/location/nearbyDriver/:passenger?radius=50"))
+                .uri(URI.create("http://0.0.0.0:8000/location/navigation/driver?passengerUid=passenger"))
                 .header("Content-Type", "application/json")
                 .GET()
                 .build();
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            System.out.println(response.body());
-            assertTrue(response.statusCode()==200);
+            System.out.println("navigationPass: " + response.statusCode() + response.body());
+            assertTrue(response.statusCode() == 200);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            assertEquals(e.getMessage(), "error");
+        }
+    }
+
+    @Test
+    public void navigationFail() {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://0.0.0.0:8000/location/navigation/notdriver?passengerUid=fakepassenger"))
+                .header("Content-Type", "application/json")
+                .GET()
+                .build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            System.out.println("navigationFail: " + response.statusCode() + response.body());
+            assertTrue(response.statusCode() == 404);
         }
         catch (Exception e){
             System.out.println(e.getMessage());
